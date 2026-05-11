109cfb73dd12e8627b7b8475a24e0a25257ac590
Checkmate-95 <organicbrandsllc@outlook.com>
2026-05-11 10:40:13 -0400

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,6 @@ import '../animations.css'
 localStorage.clear();
 sessionStorage.clear();
 
-
-// Inject Google Maps script dynamically
-const googleMapsScript = document.createElement('script');
-googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places`;
-googleMapsScript.async = true;
-document.head.appendChild(googleMapsScript);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

b0825b3da040cf3b96756c7bcb3549a3032b38b9
Checkmate-95 <organicbrandsllc@outlook.com>
2026-05-21 22:58:36 -0400

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
-import App from './App'
+import LandingPage from './landing/LandingPage'
 import './index.css'
 import '../animations.css'
 
@@ -11,7 +11,7 @@ sessionStorage.clear()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <LandingPage />
     </HelmetProvider>
   </StrictMode>,
 )

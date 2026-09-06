b604c094bdcd819b674827311063430621f09f95
Checkmate-95 <organicbrandsllc@outlook.com>
2026-09-06 13:42:38 -0400

    Register DrivePlugin + native drive engine updates

--- android/app/src/main/java/com/njdrive50/app/MainActivity.kt
+++ android/app/src/main/java/com/njdrive50/app/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.njdrive50.app
 
+import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 
-class MainActivity : BridgeActivity()
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        registerPlugin(DrivePlugin::class.java)
+        super.onCreate(savedInstanceState)
+    }
+}
